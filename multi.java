cfc03558283dc8e340091231f0801aab0de428cc
navyasri <navyasridumpala7@gmail.com>
2024-12-11 01:28:12 +0530

    multi.java content with navyasri

---
+++ multi.java
@@ -0,0 +1,14 @@
+multi.java
+public class multi {
+    public static void main(String[] args) {
+        // Directly declare and multiply the numbers
+        int num1 = 5;
+        int num2 = 4;
+       
+        // Multiply the numbers
+        int result = num1 * num2;
+       
+        // Output the result
+        System.out.println("The product is: " + result);
+    }
+}
